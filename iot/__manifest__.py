5d434f412da91f7737c5169860d1c9f03cc69d81
Nidhish jain <jain.nidhish91@gmail.com>
2020-04-15 22:20:05 +0530

    [ADD] Basic Frame

---
+++ iot/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Internet Of Things",
+    "summary": "Manage all IOT Connection",
+    "version": "12.0.1.1.0",
+    "category": "Tools",
+    "website": "https://www.shreenavkar.com",
+    "author": "navodoo",
+    "price": "29",
+    "currency": "USD",
+    "license": "LGPL-3",
+    "support": "navodoo@shreenavkar.com",
+    'description': """ This module provides management of your IoT devices inside NAVODOO.""",
+	"depends": ['web','mail'],
+    "data": [
+    
+	],
+    "images": [
+        
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
